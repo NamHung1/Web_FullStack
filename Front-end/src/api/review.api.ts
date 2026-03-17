440eb429e1f66e078776d31025a90f025306fe8a
Nam Hung <hungdngch211312@fpt.edu.vn>
2026-03-17 10:49:24 +0700

    update

--- Front-end/src/api/review.api.ts
+++ Front-end/src/api/review.api.ts
@@ -8,17 +8,8 @@ interface AddReviewPayload {
 }
 
 export const addReviewAPI = async (data: AddReviewPayload) => {
-  try {
-    const res = await api.post(`/orders/${data.orderId}/reviews`, {
-      productId: data.productId,
-      rating: data.rating,
-      comment: data.comment,
-    });
-    return res.data;
-  } catch {
-    const res = await api.post('/reviews', data);
-    return res.data;
-  }
+  const res = await api.post('/reviews', data);
+  return res.data;
 };
 
 export const getProductReviewsAPI = async (productId: string) => {
